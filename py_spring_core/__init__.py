af8c3d69149c43d556f156614157e677f0c10f96
William Chen <william_w_chen@trendmicro.com>
2025-07-05 21:16:36 +0800

    chore: Update version to 0.0.14 in __init__.py

--- py_spring_core/__init__.py
+++ py_spring_core/__init__.py
@@ -16,7 +16,7 @@ from py_spring_core.event.application_event_publisher import ApplicationEventPub
 from py_spring_core.event.commons import ApplicationEvent
 from py_spring_core.event.application_event_handler_registry import EventListener
 
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 __all__ = [
     "PySpringApplication",
