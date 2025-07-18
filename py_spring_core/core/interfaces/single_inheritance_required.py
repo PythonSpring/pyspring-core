e633109f057fde230feaf0a9e2c83ef70c4b3001
William Chen <william_w_chen@trendmicro.com>
2025-07-18 13:57:06 +0800

    Refactor SingleInheritanceRequired class to improve subclass validation
    - Updated the subclass validation logic to use a dictionary for tracking unique subclass names, ensuring only one subclass is allowed.
    - Enhanced the error message to include the count and names of found subclasses for better clarity.

--- py_spring_core/core/interfaces/single_inheritance_required.py
+++ py_spring_core/core/interfaces/single_inheritance_required.py
@@ -15,9 +15,13 @@ class SingleInheritanceRequired(Generic[T], ABC):
         """
         Check if the subclass is allowed to be inherited.
         """
-        subclasses = cls.__subclasses__()
-        if len(subclasses) > 1:
-            raise ValueError(f"Only one subclass is allowed for {cls.__name__}, but {len(subclasses)} subclasses: {[subclass.__name__ for subclass in subclasses]} found")
+        class_dict: dict[str, Type[SingleInheritanceRequired[T]]] = {}
+        for subclass in cls.__subclasses__():
+            if subclass.__name__ in class_dict:
+                continue
+            class_dict[subclass.__name__] = subclass
+        if len(class_dict) > 1:
+            raise ValueError(f"Only one subclass is allowed for {cls.__name__}, but {len(class_dict)} subclasses: {[subclass.__name__ for subclass in class_dict.values()]} found")
         
     @classmethod
     def get_subclass(cls) -> Optional[Type[T]]:
