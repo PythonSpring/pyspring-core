dfdbcddd1b242a3e05524c0f1556409715d33ea0
William Chen <william_w_chen@trendmicro.com>
2025-07-18 15:58:03 +0800

    Update version number to 0.0.19 in __init__.py

--- py_spring_core/__init__.py
+++ py_spring_core/__init__.py
@@ -18,7 +18,7 @@ from py_spring_core.event.application_event_publisher import ApplicationEventPub
 from py_spring_core.event.commons import ApplicationEvent
 from py_spring_core.event.application_event_handler_registry import EventListener
 
-__version__ = "0.0.20"
+__version__ = "0.0.19"
 
 __all__ = [
     "PySpringApplication",
