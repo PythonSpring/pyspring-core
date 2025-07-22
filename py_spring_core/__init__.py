c35bf2fc82520c40c0349dd532cb81a83d224630
William Chen <william_w_chen@trendmicro.com>
2025-07-22 14:36:19 +0800

    Add ExceptionHandler to module exports in __init__.py
    Updated the __init__.py file to include ExceptionHandler in the module exports, enhancing the accessibility of the exception handling functionality within the package.

--- py_spring_core/__init__.py
+++ py_spring_core/__init__.py
@@ -22,6 +22,7 @@ from py_spring_core.core.interfaces.graceful_shutdown_handler import GracefulShu
 from py_spring_core.event.application_event_handler_registry import EventListener
 from py_spring_core.event.application_event_publisher import ApplicationEventPublisher
 from py_spring_core.event.commons import ApplicationEvent
+from py_spring_core.exception_handler.decorator import ExceptionHandler
 
 __version__ = "0.0.24"
 
@@ -47,4 +48,5 @@ __all__ = [
     "MiddlewareConfiguration",
     "GracefulShutdownHandler",
     "ShutdownType",
+    "ExceptionHandler",
 ]
