20c65b2b9662ee0654cff636a69cc48eec9d465b
William Chen <william_w_chen@trendmicro.com>
2026-04-24 23:18:29 +0800

    Refactor ExceptionHandler decorator to use TypeVar for exception type

--- py_spring_core/exception_handler/decorator.py
+++ py_spring_core/exception_handler/decorator.py
@@ -1,18 +1,16 @@
-
-
 from functools import wraps
-from typing import Any, Callable, Type
-
+from typing import Any, Callable, Type, TypeVar
 
 from py_spring_core.exception_handler.exception_handler_registry import ExceptionHandlerRegistry
 
+E = TypeVar("E", bound=Exception)
 
-def ExceptionHandler(exception_cls: Type[Exception]) -> Callable[[Callable[[Exception], Any]], Callable]:
-    def decorator(func: Callable[[Exception], Any]) -> Callable:
+
+def ExceptionHandler(exception_cls: Type[E]) -> Callable[[Callable[[E], Any]], Callable]:
+    def decorator(func: Callable[[E], Any]) -> Callable:
         @wraps(func)
         def wrapper(*args, **kwargs) -> Any:
             return func(*args, **kwargs)
         ExceptionHandlerRegistry.register(exception_cls, wrapper)
         return wrapper
-    
     return decorator
